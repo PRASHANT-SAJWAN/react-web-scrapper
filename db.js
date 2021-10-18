f02269f401770e4692e3100bb1413435ba669863
prashant_wan <pokerosajwan0@gmail.com>
2021-10-18 23:53:40 +0530

    .

--- db.js
+++ db.js
@@ -1,6 +1,9 @@
 // connect db with server
 const Pool = require('pg').Pool;
 
+const pool = new Pool({ connectionString: 'postgres://cloxwrdtgkzjxo:ff4d2bd7b2562ef2e7fa05baea884c96ddb935a48c7562e719c0617e448ca3ce@ec2-3-226-165-74.compute-1.amazonaws.com:5432/daivolf39bfus5' });
+
+/*
 const pool = new Pool({
     user: "cloxwrdtgkzjxo",
     password: "ff4d2bd7b2562ef2e7fa05baea884c96ddb935a48c7562e719c0617e448ca3ce",
@@ -8,5 +11,5 @@ const pool = new Pool({
     port: 5432,
     database: "daivolf39bfus5"
 });
-
+*/
 module.exports = pool;
